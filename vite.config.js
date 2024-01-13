bb3acc2f6f17bf87c8415f84e32e6208e93112cb
Zandar99 <117791732+Zandar99@users.noreply.github.com>
2024-01-13 17:21:48 -0500

    update assets

--- vite.config.js
+++ vite.config.js
@@ -25,9 +25,9 @@ export default defineConfig({
     VitePWA({
       includeAssets: ["assets/*", "icons/*"],
       manifest: {
-        name: "Black Hawks Scouting",
+        name: "Metal Muscle Scouting",
         short_name: "Scouting",
-        description: "A scouting app by FRC Team 2834",
+        description: "Thank you to FRC Team 2834",
         theme_color: "#292929",
         background_color: "#292929",
         icons: [
