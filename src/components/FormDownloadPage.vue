114c58dad26e87c0ee59aa57eb5853638a5a3d95
Zandar99 <117791732+Zandar99@users.noreply.github.com>
2024-03-01 17:00:21 -0500

    reduce qrcode size

--- src/components/FormDownloadPage.vue
+++ src/components/FormDownloadPage.vue
@@ -27,7 +27,7 @@
         <input type="checkbox" v-model="excludeHeaders" id="exclude-headers" />
         <label for="exclude-headers">Exclude headers in code</label>
       </div>
-      <qrcode-vue :value="qrData" level="M" render-as="svg" :size="350" />
+      <qrcode-vue :value="qrData" level="M" render-as="svg" :size="250" />
     </div>
   </dialog>
 </template>
@@ -40,6 +40,7 @@ import { computed } from "vue";
 import QrcodeVue from "qrcode.vue";
 import { useConfigStore, useWidgetsStore } from "@/common/stores";
 import { useRouter } from "vue-router";
+
 
 const config = useConfigStore();
 const widgets = useWidgetsStore();
